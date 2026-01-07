1dca554c0776feb02c4e4a2614eec0f87c347e2d
bravetexv <lautarobravo1513@gmail.com>
2026-01-07 19:04:35 -0300

    origin/main

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chatmanager.app',
+  appName: 'chats',
+  webDir: 'dist'
+};
+
+export default config;
